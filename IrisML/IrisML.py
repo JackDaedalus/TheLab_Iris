89512e9de6b6fbc55a4ca009d1d32722a6f82a49
WIN-8F1F4NBM44D\CFinnegan <ciaran.finnegan@baesystems.com>
2019-12-17 17:49:35 +0000

    Loaded dataset, ran descriptions..

--- IrisML/IrisML.py
+++ IrisML/IrisML.py
@@ -44,3 +44,75 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+
+
+
+def MainProg_IrisML():
+
+    
+    # Set up file identifier for use in Console Print statements
+    sDataDescription = "Iris Flower Dataset" 
+    
+
+    # Load Iris Dataset
+    df_Iris = LoadIrisDataset()
+
+    # Display some basic initial statistics about dataset
+	# This data will be used to inform follow up data cleansing actions
+    DisplayBasicDataFrameInfo(df_Iris,sDataDescription)
+       
+
+    # Dummy Function used to avoid format errors in main function
+    DummyEndFunc(sDataDescription)
+
+
+
+
+def DummyEndFunc(sDatasetDescription):
+
+    print("\n\n\n")
+    print("\n\tTerminating the Python analysis for : {}..\n".format(sDatasetDescription))
+
+
+    
+
+
+def LoadIrisDataset():
+    
+    # Load dataset
+
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+    print("\n\tLoading the Dataset from URL : {}..\n".format(url))
+    
+    dataset = read_csv(url, names=names)
+
+    return dataset
+
+
+
+def DisplayBasicDataFrameInfo(df, sDatasetDescription):
+
+    print("\n\tDisplay the Dataset descriptions for : {}..\n".format(sDatasetDescription))
+
+    # shape
+    print("\n\tDisplay the Dataset shape for : {}..\n".format(sDatasetDescription))
+    print(df.shape)
+    # head
+    print("\n\tDisplay the Dataset head for : {}..\n".format(sDatasetDescription))
+    print(df.head(20))
+    # descriptions
+    print("\n\tDisplay the Dataset description for : {}..\n".format(sDatasetDescription))
+    print(df.describe())
+    # class distribution
+    print("\n\tDisplay the Dataset class distribution for : {}..\n".format(sDatasetDescription))
+    print(df.groupby('class').size())
+
+    
+
+
+
+
+# Launch the Iris ML main function
+MainProg_IrisML()
